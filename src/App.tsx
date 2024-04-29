4618070c2c70ce7dea080e482112b4712f347733
kakeru hokida <me@6mile.dev>
2024-04-29 15:42:22 +0900

    format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1 className="bg-teal-400">LinuxClub Offical Page</h1>
-  )
+  return <h1 className="bg-teal-400">LinuxClub Offical Page</h1>;
 }
 
-export default App
+export default App;
