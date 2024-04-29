879e63bc833069c15c0fba602a72017b74497c3e
kakeru hokida <me@6mile.dev>
2024-04-29 15:42:00 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="bg-teal-400">LinuxClub Offical Page</h1>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "tailwindcss";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // change port to 3000
+  server: {
+    port: 3000
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+})
